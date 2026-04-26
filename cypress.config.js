7c2f9bc8d76178bc631af6513706baeb1c0335ce
Kate Konovalova <kate.konovalova.qa@gmail.com>
2026-04-26 15:25:22 +0300

    Typescript set

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://dokasport.com.ua",
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    supportFile: "cypress/support/e2e.js",
+    specPattern: "cypress/e2e/**/*.{cy,spec,}{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.ts",
     video: false,
     defaultCommandTimeout: 10000
   },
